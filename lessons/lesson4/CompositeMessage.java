4a8609a0300502145935cfbb74a35945c639f3bd
1000001rtem <1000001rtem@gmail.com>
2018-11-11 14:52:02 +0300

    homework4

--- lessons/lesson4/CompositeMessage.java
+++ lessons/lesson4/CompositeMessage.java
@@ -1,4 +1,26 @@
 package lesson4;
 
-public class CompositeMessage {
+import lesson3.factory.Message;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CompositeMessage implements Message {
+    private List<Message> messageList = new ArrayList<>();
+    @Override
+    public void send() {
+        for(Message m:messageList){
+            m.send();
+        }
+    }
+
+    @Override
+    public String getMessage() {
+        return null;
+    }
+
+    public void addMessage(Message ... messages){
+        this.messageList.addAll(Arrays.asList(messages));
+    }
 }
